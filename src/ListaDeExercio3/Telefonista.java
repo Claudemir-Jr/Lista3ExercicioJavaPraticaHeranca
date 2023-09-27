81487678153154787b3630a3a369af3a0de51432
Claudemir Júnior <junior.codemaster@gmail.com>
2023-09-27 03:35:14 -0300

    atividade pratica Herança

---
+++ src/ListaDeExercio3/Telefonista.java
@@ -0,0 +1,20 @@
+package ListaDeExercio3;
+
+public class Telefonista extends Funcionario {
+	private String codigoEstacao;
+	
+	public Telefonista(double salario, String nome, String codigoEstacao) {
+		super(salario, nome);
+		this.codigoEstacao = codigoEstacao;
+	}
+
+	// Métodos getters e setters para código de estacao.
+	
+	public String getCodigoEstacao() {
+		return codigoEstacao;
+	}
+	
+	public void setCodigoEstacao(String codigoEstacao) {
+		this.codigoEstacao = codigoEstacao;
+	}
+}
