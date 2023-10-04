1990cc6a393cc0c30276a2d2c7593a4f66a35852
Claudemir Júnior <junior.codemaster@gmail.com>
2023-10-03 21:25:13 -0300

    Alteração na Classe Main

--- src/ListaDeExercio3/Main.java
+++ src/ListaDeExercio3/Main.java
@@ -2,13 +2,13 @@ package ListaDeExercio3;
 
 public class Main {
 	public static void main(String[] args) {
-		Gerente gerente = new Gerente(5000.0, "Maria Helena", "admin", "123456");
+		Gerente gerente = new Gerente(5650.0, "Maria Helena", "admin", "123456");
 		Telefonista telefonista = new Telefonista(2500.0, "Tauane Rafaela", "33456");
 		Secretaria secretaria = new Secretaria(3000.0, "Tiana Raquel", 101);
 		
 		System.out.println("Gerente: " + gerente.getNome() + ", Salário: " + gerente.getSalario());
 		System.out.println("Telefonista: " + telefonista.getNome() + ", Salário: " + telefonista.getSalario());
-		System.out.println("Secretaria: " + secretaria.getNome() + ", Salário: " + secretaria.getSalario());
+		System.out.println("Secretaria: " + secretaria.getNome() + ", Salário: " + secretaria.getSalario() + " Ramal: " + secretaria.getNumeroRamal());
 	}
 
 }
